9f222dd99e2968e1fd63c01064318626605992bd
momo3momo <momonouchi@mo-mo-mo.sumomo.ne.jp>
2023-09-17 15:29:03 +0900

    change

--- rebuild.js
+++ rebuild.js
@@ -24,12 +24,7 @@ class Rebuild {
 
         console.log("Finished building bindings!");
 
-        //return path.resolve(path.join("node_modules", "@serialport", "bindings", "bin", `${codeVersion.platform}-${codeVersion.arch}-${codeVersion.modules}`));
-        //return path.resolve(path.join("node_modules", "@serialport", "bindings", "build","Release"));
-        if (codeVersion.platform == "linux")
-            return path.resolve(path.join("node_modules", "@serialport", "Release"));
-        else
-            return path.resolve(path.join("node_modules", "@serialport", "bindings", "build","Release"));
+        return path.resolve(path.join("node_modules", "@serialport", "bindings", "bin", `${codeVersion.platform}-${codeVersion.arch}-${codeVersion.modules}`));
     }
 }
 
