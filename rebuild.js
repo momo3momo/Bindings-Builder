7b325bddbb65ec75adb92f60ca69800ade87d3a2
momo3momo <momonouchi@mo-mo-mo.sumomo.ne.jp>
2023-09-17 13:35:19 +0900

    path changed

--- rebuild.js
+++ rebuild.js
@@ -24,7 +24,8 @@ class Rebuild {
 
         console.log("Finished building bindings!");
 
-        return path.resolve(path.join("node_modules", "@serialport", "bindings", "bin", `${codeVersion.platform}-${codeVersion.arch}-${codeVersion.modules}`));
+        //return path.resolve(path.join("node_modules", "@serialport", "bindings", "bin", `${codeVersion.platform}-${codeVersion.arch}-${codeVersion.modules}`));
+        return path.resolve(path.join("node_modules", "@serialport", "bindings", "build","Release"));
     }
 }
 
